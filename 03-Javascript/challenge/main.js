a91dcd0b8dcbaffda2483b61d7570e8ee3953319
rachel2bester <rachel2bester@gmail.com>
2022-05-08 18:21:59 +0200

    Added isNumberPositive to main.js in js challenge

---
+++ 03-Javascript/challenge/main.js
@@ -0,0 +1,10 @@
+function isNumberPositive(number) {
+    if (number > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(isNumberPositive(-1));
+console.log(isNumberPositive(5));
